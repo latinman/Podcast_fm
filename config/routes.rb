2871d3d00b572a260572e200900f1cdb8a46ba71
latinman <erivas78@gmail.com>
2016-01-31 09:10:57 -0500

    Added dashboard for user

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   resources :podcasts, only: [:index, :show] do
     resources :episodes
   end
+
+  authenticated :podcast do
+    root "podcasts#dashboard", as: "authenticated_root"
+  end
+
   root "welcome#index"
 end
